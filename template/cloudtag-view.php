9d52d515412816be2ca503bde874da6893ee2fbb
Jim <besse.jimmy@gmail.com>
2021-09-02 17:07:56 +0200

    First version of all data with word

--- template/cloudtag-view.php
+++ template/cloudtag-view.php
@@ -1,21 +1,20 @@
 <?php
     $liste = array(
             array("name"=>"Football","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>0,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Tactique","color"=>1,"size"=>1,"x"=>5,"y"=>80,"z"=>20,"cat"=>0,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Onze","color"=>1,"size"=>1,"x"=>70,"y"=>20,"z"=>0,"cat"=>0,"nuage"=>1,"tableau"=>1),
-            array("name"=>"11","color"=>1,"size"=>1,"x"=>55,"y"=>55,"z"=>10,"cat"=>0,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Losange","color"=>1,"size"=>1,"x"=>50,"y"=>0,"z"=>-5,"cat"=>0,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Informations","color"=>1,"size"=>1,"x"=>5,"y"=>90,"z"=>10,"cat"=>0,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Tactique","color"=>4,"size"=>3,"x"=>2,"y"=>65,"z"=>15,"cat"=>0,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Onze","color"=>4,"size"=>3,"x"=>65,"y"=>15,"z"=>0,"cat"=>0,"nuage"=>1,"tableau"=>1),
+            array("name"=>"11","color"=>1,"size"=>3,"x"=>50,"y"=>46,"z"=>10,"cat"=>0,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Losange","color"=>4,"size"=>3,"x"=>40,"y"=>0,"z"=>-5,"cat"=>0,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Informations","color"=>2,"size"=>2,"x"=>5,"y"=>76,"z"=>15,"cat"=>0,"nuage"=>1,"tableau"=>1),
 
-            array("name"=>"Patience","color"=>1,"size"=>1,"x"=>30,"y"=>75,"z"=>0,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Action","color"=>1,"size"=>1,"x"=>50,"y"=>15,"z"=>15,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Recul","color"=>1,"size"=>1,"x"=>65,"y"=>30,"z"=>10,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Confrontation","color"=>1,"size"=>1,"x"=>80,"y"=>45,"z"=>20,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Équité","color"=>1,"size"=>1,"x"=>40,"y"=>65,"z"=>0,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Stratégie","color"=>1,"size"=>1,"x"=>50,"y"=>75,"z"=>0,"cat"=>1,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Éthique","color"=>1,"size"=>1,"x"=>30,"y"=>90,"z"=>-10,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Patience","color"=>4,"size"=>4,"x"=>21,"y"=>66,"z"=>-6,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Action","color"=>2,"size"=>5,"x"=>41,"y"=>20,"z"=>15,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Recul","color"=>4,"size"=>4,"x"=>60,"y"=>20,"z"=>10,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Confrontation","color"=>2,"size"=>1,"x"=>85,"y"=>34,"z"=>14,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Équité","color"=>1,"size"=>6,"x"=>37,"y"=>52,"z"=>6,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Stratégie","color"=>1,"size"=>4,"x"=>50,"y"=>65,"z"=>-10,"cat"=>1,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Éthique","color"=>2,"size"=>4,"x"=>28,"y"=>75,"z"=>-10,"cat"=>1,"nuage"=>1,"tableau"=>1),
             array("name"=>"Pragmatisme","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>1,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Sport","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>1,"nuage"=>0,"tableau"=>1),
             array("name"=>"Bon Principes","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>1,"nuage"=>0,"tableau"=>1),
             array("name"=>"Belles Valeurs","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>1,"nuage"=>0,"tableau"=>1),
 
@@ -29,43 +28,41 @@
             array("name"=>"Histoire","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>3,"nuage"=>0,"tableau"=>1),
             array("name"=>"Avenir","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>3,"nuage"=>0,"tableau"=>1),
             array("name"=>"Passé","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>3,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Future","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>3,"nuage"=>0,"tableau"=>1),
+            array("name"=>"Futur","color"=>2,"size"=>2,"x"=>0,"y"=>55,"z"=>0,"cat"=>3,"nuage"=>1,"tableau"=>1),
             array("name"=>"Projet","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>3,"nuage"=>0,"tableau"=>1),
 
             array("name"=>"Précisions","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Précision","color"=>1,"size"=>1,"x"=>80,"y"=>70,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Précision","color"=>2,"size"=>2,"x"=>85,"y"=>53,"z"=>7,"cat"=>4,"nuage"=>1,"tableau"=>1),
             array("name"=>"Détails Occurrences","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
             array("name"=>"Données","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
             array("name"=>"Statistiques","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Information","color"=>1,"size"=>1,"x"=>70,"y"=>80,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Domaines","color"=>1,"size"=>1,"x"=>10,"y"=>50,"z"=>-10,"cat"=>4,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Sujets","color"=>1,"size"=>1,"x"=>55,"y"=>5,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Détail","color"=>1,"size"=>1,"x"=>60,"y"=>60,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Information","color"=>4,"size"=>2,"x"=>74,"y"=>71,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Domaines","color"=>3,"size"=>2,"x"=>2,"y"=>42,"z"=>-10,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Sujets","color"=>1,"size"=>2,"x"=>66,"y"=>10,"z"=>15,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Détail","color"=>2,"size"=>2,"x"=>65,"y"=>58,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
             array("name"=>"Logique","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Cohérence","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>4,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Décisions","color"=>1,"size"=>1,"x"=>35,"y"=>35,"z"=>-20,"cat"=>4,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Décision","color"=>1,"size"=>1,"x"=>55,"y"=>5,"z"=>4,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Cohérence","color"=>4,"size"=>5,"x"=>29,"y"=>35,"z"=>0,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Décisions","color"=>4,"size"=>4,"x"=>12,"y"=>27,"z"=>-13,"cat"=>4,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Décision","color"=>1,"size"=>2,"x"=>57,"y"=>3,"z"=>6,"cat"=>4,"nuage"=>1,"tableau"=>1),
 
             array("name"=>"Transmission","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Existant","color"=>1,"size"=>1,"x"=>40,"y"=>10,"z"=>20,"cat"=>5,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Influences","color"=>1,"size"=>1,"x"=>70,"y"=>50,"z"=>-10,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Existant","color"=>4,"size"=>1,"x"=>33,"y"=>20,"z"=>16,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Influences","color"=>3,"size"=>1,"x"=>68,"y"=>39,"z"=>-10,"cat"=>5,"nuage"=>1,"tableau"=>1),
             array("name"=>"Singularités","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
             array("name"=>"Singularité","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Cohérence","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Original","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Rassembler","color"=>1,"size"=>1,"x"=>20,"y"=>70,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Trouver","color"=>1,"size"=>1,"x"=>40,"y"=>40,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Rassembler","color"=>1,"size"=>1,"x"=>21,"y"=>64,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Trouver","color"=>4,"size"=>1,"x"=>36,"y"=>32,"z"=>-15,"cat"=>5,"nuage"=>1,"tableau"=>1),
             array("name"=>"Communiquer","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Transmettre","color"=>1,"size"=>1,"x"=>5,"y"=>60,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Transmettre","color"=>1,"size"=>1,"x"=>0,"y"=>50,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
             array("name"=>"Vivre Ensemble","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>5,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Équilibre","color"=>1,"size"=>1,"x"=>75,"y"=>65,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Sport","color"=>1,"size"=>1,"x"=>10,"y"=>65,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Artiste","color"=>1,"size"=>1,"x"=>45,"y"=>5,"z"=>5,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Équilibre","color"=>3,"size"=>4,"x"=>75,"y"=>58,"z"=>13,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Sport","color"=>4,"size"=>6,"x"=>13,"y"=>48,"z"=>0,"cat"=>5,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Artiste","color"=>1,"size"=>1,"x"=>32,"y"=>6,"z"=>8,"cat"=>5,"nuage"=>1,"tableau"=>1),
 
             array("name"=>"Contestation","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>6,"nuage"=>0,"tableau"=>1),
             array("name"=>"Arguments","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>6,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Idées","color"=>1,"size"=>1,"x"=>30,"y"=>45,"z"=>10,"cat"=>6,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Concepts","color"=>1,"size"=>1,"x"=>50,"y"=>90,"z"=>0,"cat"=>6,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Idées","color"=>1,"size"=>3,"x"=>2,"y"=>35,"z"=>15,"cat"=>6,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Concepts","color"=>1,"size"=>2,"x"=>44,"y"=>83,"z"=>0,"cat"=>6,"nuage"=>1,"tableau"=>1),
             array("name"=>"Projet","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>6,"nuage"=>0,"tableau"=>1),
             array("name"=>"Projets","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>6,"nuage"=>0,"tableau"=>1),
             array("name"=>"Ergonomie","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>6,"nuage"=>0,"tableau"=>1),
@@ -75,19 +72,27 @@
             array("name"=>"Équipes","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>7,"nuage"=>0,"tableau"=>1),
             array("name"=>"Équipe","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>7,"nuage"=>0,"tableau"=>1),
             array("name"=>"Individualités","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>7,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Individualité","color"=>1,"size"=>1,"x"=>40,"y"=>10,"z"=>0,"cat"=>7,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Collectif","color"=>1,"size"=>1,"x"=>70,"y"=>60,"z"=>-10,"cat"=>7,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Individualité","color"=>2,"size"=>4,"x"=>30,"y"=>8,"z"=>-3,"cat"=>7,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Collectif","color"=>4,"size"=>5,"x"=>64,"y"=>42,"z"=>-11,"cat"=>7,"nuage"=>1,"tableau"=>1),
 
             array("name"=>"Spontanéité","color"=>1,"size"=>1,"x"=>0,"y"=>0,"z"=>0,"cat"=>8,"nuage"=>0,"tableau"=>1),
-            array("name"=>"Préparation","color"=>1,"size"=>1,"x"=>60,"y"=>15,"z"=>5,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Original","color"=>1,"size"=>1,"x"=>55,"y"=>40,"z"=>20,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Progrès","color"=>1,"size"=>1,"x"=>20,"y"=>70,"z"=>10,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Progrès","color"=>1,"size"=>1,"x"=>20,"y"=>70,"z"=>10,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Transcendance","color"=>1,"size"=>1,"x"=>20,"y"=>90,"z"=>20,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Plaisir","color"=>1,"size"=>1,"x"=>35,"y"=>25,"z"=>-10,"cat"=>8,"nuage"=>1,"tableau"=>1),
-            array("name"=>"Jouer","color"=>1,"size"=>1,"x"=>50,"y"=>60,"z"=>0,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Préparation","color"=>2,"size"=>1,"x"=>50,"y"=>18,"z"=>2,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Original","color"=>1,"size"=>1,"x"=>50,"y"=>33,"z"=>14,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Progrès","color"=>1,"size"=>1,"x"=>30,"y"=>62,"z"=>10,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Transcendance","color"=>2,"size"=>1,"x"=>11,"y"=>83,"z"=>15,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Plaisir","color"=>4,"size"=>1,"x"=>30,"y"=>24,"z"=>-10,"cat"=>8,"nuage"=>1,"tableau"=>1),
+            array("name"=>"Jouer","color"=>1,"size"=>1,"x"=>43,"y"=>48,"z"=>-4,"cat"=>8,"nuage"=>1,"tableau"=>1),
 
-            array("name"=>"Choix","color"=>1,"size"=>1,"x"=>55,"y"=>60,"z"=>0,"cat"=>9,"nuage"=>1,"tableau"=>1)
+            array("name"=>"Choix","color"=>2,"size"=>2,"x"=>55,"y"=>48,"z"=>3,"cat"=>9,"nuage"=>1,"tableau"=>1),
+
+            array("name"=>"Valeurs","color"=>2,"size"=>3,"x"=>18,"y"=>20,"z"=>-18,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Bons","color"=>2,"size"=>4,"x"=>18,"y"=>41,"z"=>10,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Belles","color"=>4,"size"=>3,"x"=>72,"y"=>30,"z"=>-17,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Ensemble","color"=>4,"size"=>1,"x"=>80,"y"=>37,"z"=>9,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Système","color"=>4,"size"=>1,"x"=>18,"y"=>50,"z"=>0,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Vivre","color"=>4,"size"=>1,"x"=>35,"y"=>48,"z"=>0,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Passé","color"=>4,"size"=>2,"x"=>44,"y"=>52,"z"=>5,"cat"=>10,"nuage"=>1,"tableau"=>0),
+            array("name"=>"Principes","color"=>2,"size"=>4,"x"=>59,"y"=>75,"z"=>-5,"cat"=>10,"nuage"=>1,"tableau"=>0)
 
     )
 ?>
@@ -96,10 +101,51 @@
     #cloudtag{
         position: relative;
         width: 100%;
-        height: 450px;
+        height: 400px;
+        font-size: 1.2rem;
     }
     #cloudtag .tag{
         position: absolute;
+        line-height: 100%;
+        font-family: Roboto;
+    }
+
+    #cloudtag .tag.color-1{
+        color: #78CEFC;
+    }
+    #cloudtag .tag.color-2{
+        color: #15A9F7;
+    }
+    #cloudtag .tag.color-3{
+        color: #0187EC;
+    }
+    #cloudtag .tag.color-4{
+        color: #026EC0;
+    }
+    #cloudtag .tag.color-5{
+        color: #015A9D;
+    }
+    #cloudtag .tag.color-6{
+        color: #003054;
+    }
+
+    #cloudtag .tag.s-1{
+        font-size: 70%;
+    }
+    #cloudtag .tag.s-2{
+        font-size: 120%;
+    }
+    #cloudtag .tag.s-3{
+        font-size: 150%;
+    }
+    #cloudtag .tag.s-4{
+        font-size: 220%;
+    }
+    #cloudtag .tag.s-5{
+        font-size: 280%;
+    }
+    #cloudtag .tag.s-6{
+        font-size: 320%;
     }
 </style>
 
@@ -121,6 +167,8 @@
                 case 2 :  $size = "s-2";break;
                 case 3 :  $size = "s-3";break;
                 case 4 :  $size = "s-4";break;
+                case 5 :  $size = "s-5";break;
+                case 6 :  $size = "s-6";break;
             }
 
             $class = "tag ".$color." ".$size;
